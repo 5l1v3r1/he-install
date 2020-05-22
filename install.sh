63778db834a099babd5bffa5d16d99ad3d975a30
KapoorDev <59065079+KapoorDev@users.noreply.github.com>
2020-05-22 16:58:00 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+cd
+apt install sudo
+sudo apt-get update
+sudo apt install python python2 python3 python-pip python3-pip git
+rm -rf HiddenEye
+git clone https://github.com/DarkSecDevelopers/HiddenEye
+rm _rf .git
+cd HiddenEye
+pip3 install requests
+
+python Hidden
